1045a9b252f8d503d46e2891caa744d80458e3a5
Warren Scott <59210591+M1tchster@users.noreply.github.com>
2020-01-04 18:53:56 -0500

    Img2C output files

---
+++ Demo/ImgNames.h
@@ -0,0 +1,10 @@
+//https://github.com/M1tchster/Img2C/
+#pragma once
+
+enum {
+	imgLisa,
+	imgpearl,
+	imgStarry,
+	imgVan,
+	imgTOTAL
+};
---
+++ Demo/ImgSizes.h
@@ -0,0 +1,6 @@
+//https://github.com/M1tchster/Img2C/
+#pragma once
+
+const int imgSize[] = { 
+	412,412, 410,480, 600,400, 600,475
+};
